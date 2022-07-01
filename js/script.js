a9898ef60c71871fc12ffa4390cd3c82376b87ff
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-07-01 10:21:13 +0530

    Begin the development after long time

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ $(".nav-p").hover(function(){
     $(this).toggleClass("nav-active");
     $(this).css("cursor","help");
 })
-
 $(".usr-icon").hover(function(){
     $(this).toggleClass("usr-hover");
 })
 
+//Following codes for mobile nav bar icons
 $(".m-nav").click(function(){
     $(".nav-links").fadeIn("100",function(){
         $(".nav-links").css("display","block");
